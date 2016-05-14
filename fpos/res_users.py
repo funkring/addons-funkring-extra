2ed99fc31e6a37e1b28c34f16917cb0a9082155f
Martin Reisenhofer <martin.reisenhofer@funkring.net>
2016-05-14 16:23:26 +0200

    [FIX] copy of fpos user

--- fpos/res_users.py
+++ fpos/res_users.py
@@ -23,7 +23,7 @@ from openerp.osv import fields, osv
 class res_users(osv.Model):
     _inherit = "res.users"
     _columns = {
-        "pin" : fields.char("PIN", help="User PIN for fast authentication on Fpos hardware"),
+        "pin" : fields.char("PIN", help="User PIN for fast authentication on Fpos hardware", copy=False),
         "pos_role" : fields.selection([("user","User"),
                                        ("manager","Manager"),
                                        ("admin","Administrator")], "POS Role")
