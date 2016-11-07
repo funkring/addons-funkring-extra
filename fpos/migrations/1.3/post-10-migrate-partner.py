d87bc5b629996939a4050f28f5c96162556d8dda
Martin Reisenhofer <martin.reisenhofer@funkring.net>
2016-11-07 10:19:15 +0100

    [FIX] migration

--- fpos/migrations/1.3/post-10-migrate-partner.py
+++ fpos/migrations/1.3/post-10-migrate-partner.py
@@ -2,4 +2,4 @@
 __name__ = "Correct available in pos of partner"
 
 def migrate(cr,v):
-    cr.execute("UPDATE res_partner SET available_in_pos=true WHERE id IN (SELECT o.partner_id FROM fpos_order GROUP BY 1)")
+    cr.execute("UPDATE res_partner SET available_in_pos=true WHERE id IN (SELECT o.partner_id FROM fpos_order o GROUP BY 1)")
